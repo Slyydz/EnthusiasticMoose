98d6e18845d3d53dedbca7c08ff3ea04bb6302bd
Brady Williams <williams.cole29@yahoo.com>
2021-11-12 15:11:24 -0600

    Changed structure to utilize main

--- Program.cs
+++ Program.cs
@@ -1,74 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 
-void Main()
+namespace Intro
 {
-
-    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-    Console.WriteLine("--------------------------------------------");
-
-}
-
-bool MooseAsks(string question)
-{
-    Console.Write($"{question} Y/N: ");
-    string answer = Console.ReadLine().ToLower();
-
-    while (answer != "y" && answer != "n")
+    class Program
     {
-        Console.WriteLine("Please input Y or N.");
-        answer = Console.ReadLine().ToLower();
-    }
+        static void Main(string[] args)
+        {
+            void MainTaco()
+            {
 
-    if (answer == "y")
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+                Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+                Console.WriteLine("--------------------------------------------");
 
-void MooseQuestionFunc(string question, string correct, string incorrect)
-{
-    bool isTrue = MooseAsks(question);
-    if (isTrue)
-    {
-        MooseSays(correct);
-    }
-    else
-    {
-        MooseSays(incorrect);
-    }
-}
+            }
 
-void RunMooseQuestion()
-{
-    List<string> questions = new List<string>()
+            bool MooseAsks(string question)
+            {
+                Console.Write($"{question} Y/N: ");
+                string answer = Console.ReadLine().ToLower();
+
+                while (answer != "y" && answer != "n")
+                {
+                    Console.WriteLine("Please input Y or N.");
+                    answer = Console.ReadLine().ToLower();
+                }
+
+                if (answer == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            void MooseQuestionFunc(string question, string correct, string incorrect)
+            {
+                bool isTrue = MooseAsks(question);
+                if (isTrue)
+                {
+                    MooseSays(correct);
+                }
+                else
+                {
+                    MooseSays(incorrect);
+                }
+            }
+
+            void RunMooseQuestion()
+            {
+                List<string> questions = new List<string>()
 {
     "Is Canada real?", "Are you enthusiastic?", "Do you love C# yet?", "Do you want to know a secret?"
 };
 
-    List<string> correct = new List<string>()
+                List<string> correct = new List<string>()
 {
     "Really? It seems very unlikely.", "Yay!", "Good job sucking up to your instructor!", "ME TOO!!!! I love secrets...tell me one!"
 };
 
-    List<string> incorrect = new List<string>()
+                List<string> incorrect = new List<string>()
 {
     "I  K N E W  I T !!!", "You should try it!", "You will...oh, yes, you will...", "Oh, no...secrets are the best, I love to share them!"
 };
 
-    for (int i = 0; i < questions.Count; i++)
-    {
-        MooseQuestionFunc(questions[i], correct[i], incorrect[i]);
-    }
-};
+                for (int i = 0; i < questions.Count; i++)
+                {
+                    MooseQuestionFunc(questions[i], correct[i], incorrect[i]);
+                }
+            };
 
-void MooseSays(string message)
-{
-    Console.WriteLine($@"
+            void MooseSays(string message)
+            {
+                Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -96,10 +102,13 @@ void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
+            }
+
+            MainTaco();
+
+            RunMooseQuestion();
+        }
+    }
 }
-
-Main();
-
-RunMooseQuestion();
 
 
